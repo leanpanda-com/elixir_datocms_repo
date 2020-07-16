e67cf82f6fd565c7ec2b384184bd20e35745607b
Joe Yates <joe.g.yates@gmail.com>
2020-07-16 14:51:29 +0200

    Bump version for release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule DatoCMS.Repo.MixProject do
   use Mix.Project
 
-  @version "0.8.2"
+  @version "0.8.3"
   @git_origin "https://github.com/leanpanda-com/elixir_datocms_repo"
 
   def project do
@@ -37,7 +37,7 @@ defmodule DatoCMS.Repo.MixProject do
     [
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:datocms_rest_client, "~> 0.6.0 and >= 0.6.2"},
-      {:fermo_helpers, "~> 0.8.2"},
+      {:fermo_helpers, "~> 0.8.3"},
       {:memoize, "~> 1.3"}
     ]
   end
